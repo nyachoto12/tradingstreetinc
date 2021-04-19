cceb17db4679a08abef33d4c478629519953a825
Ngonidzashe <nyachotongoni@gmail.com>
2021-04-19 16:50:49 +0200

    Adding a stubborn footer button and input

--- Trading_Street_Inc_Website/footer.php
+++ Trading_Street_Inc_Website/footer.php
@@ -39,14 +39,101 @@
         <div class="col ">
             <form class="form-inline">
                 <div class="form-row ">
+              <style>
+                  .flex-form input[type="submit"] {
+  background: #ef3f5a;
+  border: 1px solid #ef3f5a;
+  color: #fff;
+  padding: 0 30px;
+  cursor: pointer;
+  -webkit-transition: all 0.2s;
+  -moz-transition: all 0.2s;
+  transition: all 0.2s;
+}
 
-                    <div class="col p-5">
+.flex-form input[type="submit"]:hover {
+  background: #d73851;
+  border: 1px solid #d73851;
+}
 
-                        <div class="input-group mb-2">
-                            <input style="border-radius: 50px !important; width:100%;" type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
-                            <button style="border-radius: 50px !important;" class=" btn btn-primary"> &ensp;&ensp;submit&ensp;&ensp;</button>
-                        </div>
-                    </div>
+.flex-form {
+  display: -webkit-box;
+  display: flex;
+  z-index: 10;
+  position: relative;
+  width: 500px;
+  border-radius: 40px;
+}
+
+.flex-form > * {
+  border: 0;
+  padding: 0 0 0 10px;
+  background: #fff;
+  line-height: 50px;
+  font-size: 1rem;
+  border-radius: 0;
+  outline: 0;
+  -webkit-appearance: none;
+}
+
+.i{
+  flex-basis: 500px;
+}
+
+#madeby {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  padding: 25px 100px;
+  color: #fff;
+}
+
+@media all and (max-width:800px) {
+
+  .flex-form {
+    width: 100%;
+  }
+
+  .i {
+    flex-basis: 100%;
+  }
+
+  .flex-form > * {
+    font-size: 0.9rem;
+  }
+
+}
+
+@media all and (max-width:360px) {
+
+  .flex-form {
+    display: -webkit-box;
+    display: flex;
+    -webkit-box-orient: vertical;
+    -webkit-box-direction: normal;
+    flex-direction: column;
+  }
+  .i {
+    flex-basis: 0;
+  }
+
+ 
+}
+
+     
+             </style>
+                  <div class="container-fluid">
+                      <div class="row">
+                      <div class="cover">
+
+<form  class="flex-form">
+  <input class="i" type="search" placeholder="Where do you want to go?">
+  <input style="border-top-right-radius: 40px !important; border-bottom-right-radius: 40px !important;" type="submit" value="Search">
+</form>
+
+</div>
+                      </div>
+                  </div>
 
                 </div>
             </form>
