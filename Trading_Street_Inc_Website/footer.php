7a809e2dba190564711b3886c95ae08ff3aa2e2c
Ngonidzashe <nyachotongoni@gmail.com>
2021-04-20 15:54:17 +0200

    Stubborn footer

--- Trading_Street_Inc_Website/footer.php
+++ Trading_Street_Inc_Website/footer.php
@@ -33,113 +33,6 @@
       <div style="border-bottom: 1px solid rgb(226, 226, 226);"></div>
       <br>
     </div>
-<<<<<<< HEAD
-  </div>
-
-  <div class="row">
-    <div class="col ">
-      <form class="form-inline">
-        <div class="form-row ">
-          <style>
-            .flex-form input[type="submit"] {
-              background: #ef3f5a;
-              border: 1px solid #ef3f5a;
-              color: #fff;
-              padding: 0 30px;
-              cursor: pointer;
-              -webkit-transition: all 0.2s;
-              -moz-transition: all 0.2s;
-              transition: all 0.2s;
-            }
-
-            .flex-form input[type="submit"]:hover {
-              background: #d73851;
-              border: 1px solid #d73851;
-            }
-
-            .flex-form {
-              display: -webkit-box;
-              display: flex;
-              z-index: 10;
-              position: relative;
-              width: 500px;
-              border-radius: 40px;
-            }
-
-            .flex-form>* {
-              border: 0;
-              padding: 0 0 0 10px;
-              background: #fff;
-              line-height: 50px;
-              font-size: 1rem;
-              border-radius: 0;
-              outline: 0;
-              -webkit-appearance: none;
-            }
-
-            .i {
-              flex-basis: 500px;
-            }
-
-            #madeby {
-              position: absolute;
-              bottom: 0;
-              right: 0;
-              padding: 25px 100px;
-              color: #fff;
-            }
-
-            @media all and (max-width:800px) {
-
-              .flex-form {
-                width: 100%;
-              }
-
-              .i {
-                flex-basis: 100%;
-              }
-
-              .flex-form>* {
-                font-size: 0.9rem;
-              }
-
-            }
-
-            @media all and (max-width:360px) {
-
-              .flex-form {
-                display: -webkit-box;
-                display: flex;
-                -webkit-box-orient: vertical;
-                -webkit-box-direction: normal;
-                flex-direction: column;
-              }
-
-              .i {
-                flex-basis: 0;
-              }
-
-
-            }
-          </style>
-          <div class="container-fluid">
-            <div class="row">
-              <div class="cover">
-
-                <form class="flex-form">
-                  <input class="i" type="search" placeholder="Where do you want to go?">
-                  <input style="border-top-right-radius: 40px !important; border-bottom-right-radius: 40px !important;" type="submit" value="Search">
-                </form>
-
-              </div>
-            </div>
-          </div>
-
-        </div>
-      </form>
-    </div>
-  </div>
-=======
     <div class="container-fluid p-3">
     <div class="row">
         <div class="col text-center foot" style="margin-left: 370px" >
@@ -149,7 +42,7 @@
          </form>
          </div>
      </div>
->>>>>>> 6ec89c93598ea8d0a155ecc09006a2a32bc06bb9
+
 </div>
 
 <div class="container-fluid bg-dark p-4">
