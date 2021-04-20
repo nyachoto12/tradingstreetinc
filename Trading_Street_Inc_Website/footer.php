f4e9c6f1a32c8dc698c7c7f9ab94a2d6f240dc6f
Artwell-Runyararo <65966364+Artwell-Runyararo@users.noreply.github.com>
2021-04-20 19:12:45 +0200

    Services design add

--- Trading_Street_Inc_Website/footer.php
+++ Trading_Street_Inc_Website/footer.php
@@ -33,127 +33,21 @@
       <div style="border-bottom: 1px solid rgb(226, 226, 226);"></div>
       <br>
     </div>
-<<<<<<< HEAD
-
-    <div class="row">
-      <div class="col ">
-        <form class="form-inline">
-          <div class="form-row ">
-            <style>
-              .flex-form input[type="submit"] {
-                background: #ef3f5a;
-                border: 1px solid #ef3f5a;
-                color: #fff;
-                padding: 0 30px;
-                cursor: pointer;
-                -webkit-transition: all 0.2s;
-                -moz-transition: all 0.2s;
-                transition: all 0.2s;
-              }
-
-              .flex-form input[type="submit"]:hover {
-                background: #d73851;
-                border: 1px solid #d73851;
-              }
-
-              .flex-form {
-                display: -webkit-box;
-                display: flex;
-                z-index: 10;
-                position: relative;
-                width: 500px;
-                border-radius: 40px;
-              }
-
-              .flex-form>* {
-                border: 0;
-                padding: 0 0 0 10px;
-                background: #fff;
-                line-height: 50px;
-                font-size: 1rem;
-                border-radius: 0;
-                outline: 0;
-                -webkit-appearance: none;
-              }
-
-              .i {
-                flex-basis: 500px;
-              }
-
-              #madeby {
-                position: absolute;
-                bottom: 0;
-                right: 0;
-                padding: 25px 100px;
-                color: #fff;
-              }
-
-              @media all and (max-width:800px) {
-
-                .flex-form {
-                  width: 100%;
-                }
-
-                .i {
-                  flex-basis: 100%;
-                }
-
-                .flex-form>* {
-                  font-size: 0.9rem;
-                }
-
-              }
-
-              @media all and (max-width:360px) {
-
-                .flex-form {
-                  display: -webkit-box;
-                  display: flex;
-                  -webkit-box-orient: vertical;
-                  -webkit-box-direction: normal;
-                  flex-direction: column;
-                }
-
-                .i {
-                  flex-basis: 0;
-                }
-
-
-              }
-            </style>
-            <div class="container-fluid">
-              <div class="row">
-                <div class="cover">
-
-                  <form class="flex-form">
-                    <input class="i" type="search" placeholder="Where do you want to go?">
-                    <input style="border-top-right-radius: 40px !important; border-bottom-right-radius: 40px !important;" type="submit" value="Search">
-                  </form>
-
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </form>
-      </div>
-=======
     <div class="container-fluid p-3">
-    <div class="row">
-        <div class="col text-center foot" style="margin-left: 370px" >
-         <form  class="">
-           <input class="i form-control" type="search" placeholder="Your email address" style=" position : relative; width : 589px; height: 50px; top : 16px;  " > 
-           <span ><input style=" position : relative; top : -35px; left : 43%; width:150px; height: 51px; border-bottom-right-radius: 30px !important; border-top-right-radius: 30px !important; background-color : #17A2B8; color : white" type="submit" value="Submit" class="form-control"></span>
-         </form>
-         </div>
-     </div>
+      <div class="row">
+        <div class="col text-center foot" style="margin-left: 370px">
+          <form class="">
+            <input class="i form-control" type="search" placeholder="Your email address" style=" position : relative; width : 589px; height: 50px; top : 16px;  ">
+            <span><input style=" position : relative; top : -35px; left : 43%; width:150px; height: 51px; border-bottom-right-radius: 30px !important; border-top-right-radius: 30px !important; background-color : #17A2B8; color : white" type="submit" value="Submit" class="form-control"></span>
+          </form>
+        </div>
+      </div>
 
-</div>
-
-<div class="container-fluid bg-dark p-4">
-  <div class="row justify-content-center">
-    <div class="col d-flex justify-content-center text-white">
-      <h6>All Copy Rights Reserved &copy; Trading Street Inc.</h6>
->>>>>>> 7a809e2dba190564711b3886c95ae08ff3aa2e2c
     </div>
-  </div>
+
+    <div class="container-fluid bg-dark p-4">
+      <div class="row justify-content-center">
+        <div class="col d-flex justify-content-center text-white">
+          <h6>All Copy Rights Reserved &copy; Trading Street Inc.</h6>
+        </div>
+      </div>
