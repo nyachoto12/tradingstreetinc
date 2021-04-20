460aa8fda5c3351a0d92b096eacedacd10a9b8db
Artwell-Runyararo <65966364+Artwell-Runyararo@users.noreply.github.com>
2021-04-20 13:38:25 +0200

    Old files

--- Trading_Street_Inc_Website/footer.php
+++ Trading_Street_Inc_Website/footer.php
@@ -2,151 +2,150 @@
 <html>
 <!-- Footer -->
 <div style="background-color: #F8FEFF;" class="container-fluid">
-    <br>
-    <div class="row">
-        <div class="col d-flex justify-content-center p-5">
-            <h5 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-weight:bolder;" class="text-uppercase text-dark">Trading <strong class="info-color">Street Inc</strong></h5>
-        </div>
+  <br>
+  <div class="row">
+    <div class="col d-flex justify-content-center p-5">
+      <h5 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;font-weight:bolder;" class="text-uppercase text-dark">Trading <strong class="info-color">Street Inc</strong></h5>
     </div>
-    <div class="row">
-        <div class="col">
-            <br>
-            <div style="border-bottom: 1px solid rgb(226, 226, 226);"></div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <br>
+      <div style="border-bottom: 1px solid rgb(226, 226, 226);"></div>
 
-            <div class="container-fluid p-3">
-                <div class="row">
-                    <div class="col border-end">
-                        <h6 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;font-weight:bolder;" class="text-center text-dark">Our Address</h6>
-                        <p class="text-center">5364 Harare Road Streer 7</p>
-                    </div>
+      <div class="container-fluid p-3">
+        <div class="row">
+          <div class="col border-end">
+            <h6 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;font-weight:bolder;" class="text-center text-dark">Our Address</h6>
+            <p class="text-center">5364 Harare Road Streer 7</p>
+          </div>
 
-                    <div class="col border-end">
-                        <h6 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;font-weight:bolder;" class="text-center text-dark">Talk To Expert</h6>
-                        <p class="text-center">+263 774 546 0030 or +263 713 674 9737</p>
-                    </div>
-                    <div class="col">
-                        <h6 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;font-weight:bolder;" class="text-center text-dark">Email Us</h6>
-                        <p class="text-center">email@gmail.co.zw</p>
-                    </div>
-                </div>
+          <div class="col border-end">
+            <h6 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;font-weight:bolder;" class="text-center text-dark">Talk To Expert</h6>
+            <p class="text-center">+263 774 546 0030 or +263 713 674 9737</p>
+          </div>
+          <div class="col">
+            <h6 style="font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ;font-weight:bolder;" class="text-center text-dark">Email Us</h6>
+            <p class="text-center">email@gmail.co.zw</p>
+          </div>
+        </div>
+      </div>
+      <div style="border-bottom: 1px solid rgb(226, 226, 226);"></div>
+      <br>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col ">
+      <form class="form-inline">
+        <div class="form-row ">
+          <style>
+            .flex-form input[type="submit"] {
+              background: #ef3f5a;
+              border: 1px solid #ef3f5a;
+              color: #fff;
+              padding: 0 30px;
+              cursor: pointer;
+              -webkit-transition: all 0.2s;
+              -moz-transition: all 0.2s;
+              transition: all 0.2s;
+            }
+
+            .flex-form input[type="submit"]:hover {
+              background: #d73851;
+              border: 1px solid #d73851;
+            }
+
+            .flex-form {
+              display: -webkit-box;
+              display: flex;
+              z-index: 10;
+              position: relative;
+              width: 500px;
+              border-radius: 40px;
+            }
+
+            .flex-form>* {
+              border: 0;
+              padding: 0 0 0 10px;
+              background: #fff;
+              line-height: 50px;
+              font-size: 1rem;
+              border-radius: 0;
+              outline: 0;
+              -webkit-appearance: none;
+            }
+
+            .i {
+              flex-basis: 500px;
+            }
+
+            #madeby {
+              position: absolute;
+              bottom: 0;
+              right: 0;
+              padding: 25px 100px;
+              color: #fff;
+            }
+
+            @media all and (max-width:800px) {
+
+              .flex-form {
+                width: 100%;
+              }
+
+              .i {
+                flex-basis: 100%;
+              }
+
+              .flex-form>* {
+                font-size: 0.9rem;
+              }
+
+            }
+
+            @media all and (max-width:360px) {
+
+              .flex-form {
+                display: -webkit-box;
+                display: flex;
+                -webkit-box-orient: vertical;
+                -webkit-box-direction: normal;
+                flex-direction: column;
+              }
+
+              .i {
+                flex-basis: 0;
+              }
+
+
+            }
+          </style>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="cover">
+
+                <form class="flex-form">
+                  <input class="i" type="search" placeholder="Where do you want to go?">
+                  <input style="border-top-right-radius: 40px !important; border-bottom-right-radius: 40px !important;" type="submit" value="Search">
+                </form>
+
+              </div>
             </div>
-            <div style="border-bottom: 1px solid rgb(226, 226, 226);"></div>
-            <br>
+          </div>
+
         </div>
+      </form>
     </div>
-
-    <div class="row">
-        <div class="col ">
-            <form class="form-inline">
-                <div class="form-row ">
-              <style>
-                  .flex-form input[type="submit"] {
-  background: #ef3f5a;
-  border: 1px solid #ef3f5a;
-  color: #fff;
-  padding: 0 30px;
-  cursor: pointer;
-  -webkit-transition: all 0.2s;
-  -moz-transition: all 0.2s;
-  transition: all 0.2s;
-}
-
-.flex-form input[type="submit"]:hover {
-  background: #d73851;
-  border: 1px solid #d73851;
-}
-
-.flex-form {
-  display: -webkit-box;
-  display: flex;
-  z-index: 10;
-  position: relative;
-  width: 500px;
-  border-radius: 40px;
-}
-
-.flex-form > * {
-  border: 0;
-  padding: 0 0 0 10px;
-  background: #fff;
-  line-height: 50px;
-  font-size: 1rem;
-  border-radius: 0;
-  outline: 0;
-  -webkit-appearance: none;
-}
-
-.i{
-  flex-basis: 500px;
-}
-
-#madeby {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  padding: 25px 100px;
-  color: #fff;
-}
-
-@media all and (max-width:800px) {
-
-  .flex-form {
-    width: 100%;
-  }
-
-  .i {
-    flex-basis: 100%;
-  }
-
-  .flex-form > * {
-    font-size: 0.9rem;
-  }
-
-}
-
-@media all and (max-width:360px) {
-
-  .flex-form {
-    display: -webkit-box;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    flex-direction: column;
-  }
-  .i {
-    flex-basis: 0;
-  }
-
- 
-}
-
-     
-             </style>
-                  <div class="container-fluid">
-                      <div class="row">
-                      <div class="cover">
-
-<form  class="flex-form">
-  <input class="i" type="search" placeholder="Where do you want to go?">
-  <input style="border-top-right-radius: 40px !important; border-bottom-right-radius: 40px !important;" type="submit" value="Search">
-</form>
-
-</div>
-                      </div>
-                  </div>
-
-                </div>
-            </form>
-        </div>
-    </div>
+  </div>
 </div>
 
 <div class="container-fluid bg-dark p-4">
-    <div class="row justify-content-center">
-        <div class="col d-flex justify-content-center text-white">
-            <h6>All Copy Rights Reserved &copy; Trading Street Inc.</h6>
-        </div>
+  <div class="row justify-content-center">
+    <div class="col d-flex justify-content-center text-white">
+      <h6>All Copy Rights Reserved &copy; Trading Street Inc.</h6>
     </div>
+  </div>
 </div>
 <!-- End Of Footer -->
 
