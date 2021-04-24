77bc0596d2147accae399844f5d821c087a1fe12
Artwell-Runyararo <65966364+Artwell-Runyararo@users.noreply.github.com>
2021-04-24 20:57:09 +0200

    push push

--- Trading_Street_Inc_Website/news.php
+++ Trading_Street_Inc_Website/news.php
@@ -297,16 +297,17 @@
         <div class="row no-gutters p-0 m-0">
             <!-- Side Bar -->
             <?php
-                $a = "". date("Y/m/d");
-                $date = new DateTime();
-                $n = $date->modify("-1 days")->format('Y/m/d');
-                ?>
+            $a = "" . date("Y/m/d");
+            $date = new DateTime();
+            $n = $date->modify("-1 days")->format('Y/m/d');
+
+            ?>
             <?php
-            $api_url = "https://newsapi.org/v2/everything?q=trading&from=$n&to=$a&sortBy=date&apiKey=d1ab4131a93b4ca3bcda10cd56bbf08b";
+            $api_url = "https://newsapi.org/v2/everything?q=forex&from=$a&to=$a&sortBy=date&apiKey=d1ab4131a93b4ca3bcda10cd56bbf08b";
             $newlist = file_get_contents($api_url);
             $newlist = json_decode($newlist);
 
-            $break_out   = 12;
+            $break_out   = 50;
             $counter     = 0;
 
             foreach ($newlist->articles as $news) {
@@ -373,16 +374,15 @@
 
 
                             <?php
-                            if (file_exists('news.json')) {
-                                $api_url = 'news.json';
-                                $newlist = json_decode(file_get_contents($api_url));
-                            } else {
-                                $news_key = 'forex'; // we will be fetching only sports news related
-                                $api_url = 'https://newsapi.org/v2/everything?q=' . $news_key . '&from=2021-04-21&to=2021-04-20&sortBy=popularity&apiKey=e13ea32f8a5344e7bce102118a253256';
-                                $newlist = file_get_contents($api_url);
-                                file_put_contents('news.json', $newlist);
-                                $newlist = json_decode($newlist);
-                            }
+
+                            $date = new DateTime();
+                            $n = $date->modify("-1 days")->format('Y/m/d');
+
+                            ?>
+                            <?php
+                            $api_url = "https://newsapi.org/v2/everything?q=forex&from=$n&to=$n&sortBy=date&apiKey=d1ab4131a93b4ca3bcda10cd56bbf08b";
+                            $newlist = file_get_contents($api_url);
+                            $newlist = json_decode($newlist);
 
                             foreach ($newlist->articles as $news) { ?> <div class="row w-100 signle-news">
 
